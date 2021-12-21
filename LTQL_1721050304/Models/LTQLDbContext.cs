a00966301497eeb0a873dd7fe0b7bd9707c7a733
TranThiHue999 <tranthihue20899@gmail.com>
2021-12-21 15:05:56 +0700

    First_commit_TTH304

---
+++ LTQL_1721050304/Models/LTQLDbContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace LTQL_1721050304.Models
+{
+    public partial class LTQLDbContext : DbContext
+    {
+        public LTQLDbContext()
+            : base("name=LTQLDbContext")
+        {
+        }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
